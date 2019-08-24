3e91074986196406344395e7638d452cfcde4f8a
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-08-24 17:44:38 +0900

    コントローラーの編集（JSON形式のデータのやり取り）

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,2 +1,49 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class WeathersController < ApplicationController
+  def index
+    uri = URI.parse("http://192.168.33.10:8000/")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request.body = JSON.dump({
+      "city": "横浜"
+    })
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    begin
+      # responseの値に応じて処理を分ける
+      case response
+      # 成功した場合
+      when Net::HTTPSuccess
+        # responseのbody要素をJSON形式で解釈し、hashに変換
+        result = JSON.parse(response.body)
+        # 表示用の変数に結果を格納
+        @header = result["header"]
+        @record = result["record"]
+      # 別のURLに飛ばされた場合
+      when Net::HTTPRedirection
+        @message = "Redirection: code=#{response.code} message=#{response.message}"
+      # その他エラー
+      else
+        @message = "HTTP ERROR: code=#{response.code} message=#{response.message}"
+      end
+    # エラー時処理
+    rescue IOError => e
+      @message = "e.message"
+    rescue TimeoutError => e
+      @message = "e.message"
+    rescue JSON::ParserError => e
+      @message = "e.message"
+    rescue => e
+      @message = "e.message"
+    end
+  end
 end
